85c10a7f47a01f488706ff843224b7ea500ca4cc
cricrisula <48505827+cricrisula@users.noreply.github.com>
2019-09-12 00:35:21 -0600

    Codigo de Barras
    generador de codigo de barras

---
+++ Generador_QR/index.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Generador_QR.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <div>
+            <asp:TextBox ID="txtCode" runat="server"></asp:TextBox>
+            <asp:Button ID="btnGenerate" runat="server" Text="Generate" OnClick="btnGenerate_Click"  />
+            <hr />
+            </div>
+        <div>
+            <img runat="server" id="imgCtrl"   />
+    </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Generador_QR/index.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.IO;
+using System.Drawing;
+using MessagingToolkit.QRCode.Codec;
+
+namespace Generador_QR
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGenerate_Click(object sender, EventArgs e)
+        {
+            QRCodeEncoder encoder = new QRCodeEncoder();
+            Bitmap img = encoder.Encode(txtCode.Text);
+            System.Drawing.Image QR = (System.Drawing.Image)img;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                QR.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                byte[] imageBytes = ms.ToArray();
+                imgCtrl.Src = "data:image/gif;base64," + Convert.ToBase64String(imageBytes);
+                imgCtrl.Height = 200;
+                imgCtrl.Width = 200;
+                
+            }
+
+        }
+    }
+}
